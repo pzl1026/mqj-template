e3f273cf79d6353f7d449b35a28dfbc19b87969c
undefined <undefined>
2020-10-27 16:10:55 +0800

    tijiao

--- common/src/index.js
+++ common/src/index.js
@@ -1,13 +1,13 @@
-// if (!location.hash) {
-//   let recent = JSON.parse(localStorage.getItem('_mqj_recent'));
-//   if (recent) {
-//     window.location.href = recent[0].href;
-//   } else {
-//     window.location.href = '/goods#/goods/list';
-//   }
-// } else {
+if (!location.hash) {
+  let recent = JSON.parse(localStorage.getItem('_mqj_recent'));
+  if (recent) {
+    window.location.href = recent[0].href;
+  } else {
+    window.location.href = '/goods#/goods/list';
+  }
+} else {
+  import("./bootstrap");
+}
 
-// }
-import("./bootstrap");
  
 
